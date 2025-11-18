fed3bcf4c08d6cbe54d912f31351401c95c99243
FireLifeDanik28 <lifedanik2006@gmail.com>
2025-11-18 11:42:40 +0100

    Add project files.

---
+++ Csharp/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Csharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //początek
+            int arrNumsWynik = 0;
+            int[] arrNums = new int[] { 5, 10, 15, 20, 25 };
+            for (int i = 0; i < arrNums.Length; i++) {
+                arrNumsWynik = arrNumsWynik + arrNums[i];
+            }
+            Console.WriteLine(arrNumsWynik); //75
+        }
+    }
+}
